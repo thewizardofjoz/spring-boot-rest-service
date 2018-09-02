3e0bc6b4b74c9fb6049e5a3c1f65f09fac32e2cf
Jose <josesmithesh@gmail.com>
2018-09-02 15:31:01 +0530

    initial project from start.spring.io

---
+++ src/test/java/com/github/thewizardofjoz/springbootrestservice/SpringBootRestServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.thewizardofjoz.springbootrestservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRestServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
